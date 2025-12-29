7b88a4e998cd9a0d67e33a12f8a455047ff08f3c
dragonblade316 <dragonblade316@gmail.com>
2025-12-29 11:16:12 -0700

    Basic camera pub powered by nokhwa. currently stuck at 15 fps for some reason

--- evocam/src/main.rs
+++ evocam/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use image::codecs::png::PngEncoder;
-use nokhwa::{Camera, pixel_format::RgbFormat, utils::{CameraIndex, RequestedFormat, RequestedFormatType}};
+use nokhwa::{Camera, pixel_format::RgbFormat, utils::{CameraFormat, CameraIndex, RequestedFormat, RequestedFormatType, Resolution}};
 
 #[tokio::main]
 async fn main() {
@@ -11,21 +11,34 @@ async fn main() {
     let session = evobridge::Session::new(None, None).await;
     let publisher = session.get_publisher("evocam/cam0").await;
 
+    publisher.get_matching_listener().await.with_mut_callback(|m| {
+        match m.matching() {
+            true => println!("an entity is matching"),
+            false => println!("there is no entity matching")
+        };
+    });
+
+    use embedded_fps::{FPS, StdClock};
+    let std_clock = StdClock::default();
+    let mut fps_counter = embedded_fps::FPS::<60, _>::new(std_clock);
+
     // request the absolute highest resolution CameraFormat that can be decoded to RGB.
+    let format = CameraFormat::new(Resolution::new(1920, 1080), nokhwa::utils::FrameFormat::GRAY, 60);
+    
     let requested = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
     // make the camera
     let mut camera = Camera::new(index, requested).unwrap();
     camera.open_stream().unwrap();
 
+    println!("{}", camera.camera_format());
+
     loop {
         let frame = camera.frame().unwrap();   
         let decoded: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = frame.decode_image::<RgbFormat>().unwrap();
-        let _ = publisher.put(evotypes_rs::evosoft::image::Image::from_image(decoded)).await;
+        let img =  evotypes_rs::evosoft::image::Image::from_image(decoded);
+
+        let _ = publisher.put(img).await.expect("there is no err case");
+
+        println!("{}", fps_counter.tick());
     }
-    // get a frame
-    let frame = camera.frame().unwrap();
-    // println!("Captured Single Frame of {}", frame.buffer().len());
-    // // decode into an ImageBuffer
-    // let decoded: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = frame.decode_image::<RgbFormat>().unwrap();
-    // println!("Decoded Frame of {}", decoded.len());
 }
