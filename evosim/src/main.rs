2c16079d49a58ee497439b3dc2f8990b8d51bee8
dragonblade316 <dragonblade316@gmail.com>
2026-01-18 12:29:30 -0700

    Revert "evosim mujoco version (only doing this to save my work)"
    This reverts commit 00fa84eb047a0f2dbcfca6aa0434a70b0602f5f1.

--- evosim/src/main.rs
+++
@@ -1,9 +0,0 @@
-use bevy::{DefaultPlugins, app::App};
-use bevy_stl::StlPlugin;
-use bevy_urdf::URDFPlugin;
-
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins((URDFPlugin::default(), StlPlugin));
-}
